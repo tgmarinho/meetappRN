26ef6accb65db47d58c5c990acb0f4476fc95406
Thiago Marinho <tgmarinho@gmail.com>
2019-10-27 22:01:32 -0400

    MeetApp RN - Infinite Scroll List

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withNavigationFocus } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import pt from 'date-fns/locale/pt';
 import { format, parseISO, subDays, addDays } from 'date-fns';
 import { showMessage } from 'react-native-flash-message';
 import PropTypes from 'prop-types';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import api from '~/services/api';
 import Header from '~/components/Header';
 import Background from '~/components/Background';
@@ -13,45 +14,92 @@ import Empty from '~/components/Empty';
 import Card from '~/components/Card';
 import { Container, DateContainer, DateLabel, List, Button } from './styles';
 
+const styles = StyleSheet.create({
+  list: {
+    paddingHorizontal: 20,
+  },
+
+  listItem: {
+    backgroundColor: '#EEE',
+    marginTop: 20,
+    padding: 30,
+  },
+
+  loading: {
+    alignSelf: 'center',
+    marginVertical: 20,
+  },
+});
+
 Icon.loadFont();
 
 function Dashboard({ isFocused }) {
-  const [meetups, setMeetups] = useState([]);
   const [date, setDate] = useState(new Date());
-  const [loading, setLoading] = useState(true);
+  const [meetups, setMeetups] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const dateFormatted = useMemo(
-    () => format(date, "d 'de' MMMM", { locale: pt }),
-    [date]
-  );
+  async function loadMeetups(pageNumber = page, shouldRefresh = false) {
+    if (!hasMore) return;
+    if (loading) return;
+
+    setLoading(true);
+
+    const response = await api.get('meetups', {
+      params: { date, page },
+    });
+    const data = response.data.map(meet => {
+      return {
+        ...meet,
+        date: format(parseISO(meet.date), "d 'de' MMMM', às ' h'h'", {
+          locale: pt,
+        }),
+      };
+    });
+
+    setLoading(false);
+    setHasMore(data.length >= 10);
+    setPage(pageNumber + 1);
+
+    setMeetups(shouldRefresh ? data : [...meetups, ...data]);
+  }
+
+  async function refreshList() {
+    setPage(1);
+    setRefreshing(true);
+    await loadMeetups(1, true);
+
+    setRefreshing(false);
+  }
 
   useEffect(() => {
-    async function loadMeetups() {
-      setLoading(true);
-      const response = await api.get('meetups', {
-        params: { date },
-      });
-      const data = response.data.map(meetup => {
-        return {
-          ...meetup,
-          date: format(parseISO(meetup.date), "d 'de' MMMM', às ' h'h'", {
-            locale: pt,
-          }),
-        };
-      });
-      setMeetups(data);
-      setLoading(false);
-    }
-
     loadMeetups();
-  }, [date, isFocused]);
+  }, [date, isFocused]); // eslint-disable-line
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
+    setPage(1);
+    setHasMore(true);
+    setMeetups([]);
   }
 
   function handleNextDay() {
     setDate(addDays(date, 1));
+    setPage(1);
+    setHasMore(true);
+    setMeetups([]);
+  }
+
+  function renderFooter() {
+    if (!loading) return null;
+
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator />
+      </View>
+    );
   }
 
   async function handleSubscription(id) {
@@ -81,7 +129,7 @@ function Dashboard({ isFocused }) {
             <Icon name="chevron-left" size={40} color="#fff" />
           </Button>
 
-          <DateLabel>{dateFormatted}</DateLabel>
+          <DateLabel>{format(date, "d 'de' MMMM", { locale: pt })}</DateLabel>
 
           <Button onPress={handleNextDay}>
             <Icon name="chevron-right" size={40} color="#fff" />
@@ -95,9 +143,14 @@ function Dashboard({ isFocused }) {
             renderItem={({ item }) => (
               <Card data={item} register={() => handleSubscription(item.id)} />
             )}
+            onRefresh={refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.1}
+            onEndReached={() => loadMeetups()}
+            ListFooterComponent={() => renderFooter()}
           />
         ) : (
-          <Empty message="Sem eventos nessa data!" />
+          !loading && <Empty message="Sem eventos nessa data!" />
         )}
       </Container>
     </Background>
